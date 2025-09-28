3e993328ff4f91252b079f8fee4c8fb5c0863889
HARRMOS <108294489+HARRMOS@users.noreply.github.com>
2025-09-28 12:35:09 +0200

    Refactor user sync functions for PostgreSQL

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 import bodyParser from 'body-parser';
 import { 
-  syncUserToMySQL,
+  syncUserToPostgres,
+  pgPool,
   findOrCreateUser,
   findUserById,
   checkGlobalChatbotQuota,
   incrementChatbotMessagesUsed,
   getUserStats,
-  mysqlPool, // <-- Ajouté ici
-  updateConversationTitleMySQL,
-  deleteConversationMySQL,
-  getConversationsForUserBot, // Ajouté
+  updateConversationTitle,
+  deleteConversation,
+  getConversationsForUserBot,
   getBotById,
-  getMessagesForUserBot, // Ajouté
-  getUserBotPreferences, // Ajouté
+  getMessagesForUserBot,
+  getUserBotPreferences,
   saveQuizResult,
   getQuizResultsForUser,
   setMaintenance,
